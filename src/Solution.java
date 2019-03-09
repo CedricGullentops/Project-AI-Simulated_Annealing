06c73114184b9b9888d6e718791ae240ee84f371
Laurens Van de Walle <laurens.vandewalle@student.kuleuven.be>
2019-03-09 18:26:55 +0100

    Created initial solution method in Solutions class

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,160 @@
-
+import java.util.ArrayList;
 
 public class Solution {
+	private int cost;
+	private ArrayList<int[]> Vehicle_Assignements;
+	private ArrayList<int[]> Assigned_Requests;
+	private ArrayList<int[]> Unassigned_Requests;
+	
+	/**
+	 * Constructor
+	 */
+	public Solution(){
+		Vehicle_Assignements = new ArrayList<int[]>(); 	// 0 = car, 1 = zone
+		Assigned_Requests = new ArrayList<int[]>();		// 0 = id, 1 = car
+		Unassigned_Requests = new ArrayList<int[]>();	// 0 = id
+	}
+	
+	
+	/**
+	 * Generate an initial solution
+	 */
+	public void GenerateInitial(ArrayList<Request> requests, OverlapMatrix matrix)
+	{
+		// variables used in function
+		int i, j, k, l;
+		int currentCar, carZone;
+		boolean possible, inZone, carPossible;
+		Request currentRequest;
+		int[] currentAssignedRequest, currentAssignedVehicle, assignement, unassignement;
+		assignement = new int[2];
+		unassignement = new int[1];
+		// iterate over each element in requests
+		for (i = 0 ; i < requests.size() ; i++)
+		{
+			possible = false;
+			inZone = true;
+			currentRequest = requests.get(i);
+			// iterate over each car in current request
+			for (k = 0 ; k < currentRequest.getCars().size() ; k ++)
+			{
+				if (possible = true)
+				{
+					continue;
+				}
+				carPossible = true;
+				currentCar = currentRequest.getCars().get(k);
+				// iterate over each assigned request
+				for (j = 0 ; j < this.getAssigned_Requests().size() ; j++)
+				{
+					currentAssignedRequest = this.getAssigned_Requests().get(j);
+					// check if request j uses this car
+					if(currentAssignedRequest[1] == currentCar)
+					{
+						// check if current request and assigned request overlap in time
+						if (matrix.get(currentRequest.getId(), currentAssignedRequest[0]) == true)
+						{
+							carPossible = false;
+						}
+					}
+				}
+				// if car is possible and car has no zone or is in neighbouring zone assign car to request
+				if(carPossible)
+				{
+					carZone = -1;
+					// iterate over assigned vehicles
+					for (l = 0 ; l < this.getVehicle_Assignements().size() ; l++)
+					{
+						currentAssignedVehicle = this.getVehicle_Assignements().get(l);
+						// check if this is the vehicle in question
+						if(currentCar == currentAssignedVehicle[0])
+						{
+							carZone = currentAssignedVehicle[1];
+							break;
+						}
+					}
+					// if car has no zone yet assign the requests zone and assign car to request
+					if(carZone == -1)
+					{
+						// assign zone to car
+						assignement[0] = currentCar;
+						assignement[1] = currentRequest.getZone();
+						this.getVehicle_Assignements().add(assignement);
+						// assign car to request
+						assignement[0] = currentRequest.getId();
+						assignement[1] = currentCar;
+						this.getAssigned_Requests().add(assignement);
+						// set possible to true and continue to next request
+						possible = true;
+						continue;
+					}
+					else
+					{
+						// if cars zone is impossible add to cost and continue to next car
+						if (Math.abs(currentRequest.getZone() - carZone) > 1)
+						{
+							continue;
+						}
+						// if cars zone is possible assign car to request and continue to next request
+						else
+						{
+							// if cars zone is next to requests zone
+							if (currentRequest.getZone() != carZone)
+							{
+								this.setCost(this.getCost() + currentRequest.getP2());
+							}
+							assignement[0] = currentRequest.getId();
+							assignement[1] = currentCar;
+							this.getAssigned_Requests().add(assignement);
+							// set possible to true and continue to next request
+							possible = true;
+							continue;
+						}
+					}
+				}
+			}
+			// if request is not possible add to unassigned and add cost
+			if(!possible)
+			{
+				unassignement[0] = currentRequest.getId();
+				this.getUnassigned_Requests().add(unassignement);
+			}
+		}
+	}
+	
 
+	/**
+	 * Getters and setters (Automatically generated)
+	 */
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public ArrayList<int[]> getVehicle_Assignements() {
+		return Vehicle_Assignements;
+	}
+
+	public void setVehicle_Assignements(ArrayList<int[]> vehicle_Assignements) {
+		Vehicle_Assignements = vehicle_Assignements;
+	}
+
+	public ArrayList<int[]> getAssigned_Requests() {
+		return Assigned_Requests;
+	}
+
+	public void setAssigned_Requests(ArrayList<int[]> assigned_Requests) {
+		Assigned_Requests = assigned_Requests;
+	}
+
+	public ArrayList<int[]> getUnassigned_Requests() {
+		return Unassigned_Requests;
+	}
+
+	public void setUnassigned_Requests(ArrayList<int[]> unassigned_Requests) {
+		Unassigned_Requests = unassigned_Requests;
+	}
 }
